0b984b386fb4088bb5a7ddf214bae44139b6e2b1
Chris Troutner <chris.troutner@gmail.com>
2025-04-24 18:33:10 -0700

    Adding link to astral

--- 13-convert-eventid-to-note/eventId-to-noteId.js
+++ 13-convert-eventid-to-note/eventId-to-noteId.js
@@ -7,7 +7,9 @@ import * as nip19 from 'nostr-tools/nip19'
 
 // Copied a random npub from Nostr
 // const npub = 'npub1eg59l0mdz29d7srvg9kr8g76lspy9effny25qp425sl2mhcqfmeqqrpnw6'
-const eventId = '8f9e81e4c6905c0d1debf5ebc4b220bd9604dd0d810d0815085365acfec00711'
+const eventId = '9ec3877eb681ddbc64beb67cbf336757533e62335aace7da70e2c07c041e296a'
 
 const result = nip19.noteEncode(eventId)
 console.log('result: ', result)
+
+console.log(`https://astral.psfoundation.info/${result}`)
